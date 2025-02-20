301ef3de30f03886bff93380a2d4bcc801876e95
sanduni4822 <sanduniwathsala0123@gmail.com>
2025-02-20 11:08:25 +0530

    Modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import Navbar from "./components/Navbar"; 
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/homepage/Homepage";
 
 function App() {
   return (
-    <div>
+    <div className="bg-gradient-to-r from-[#1e0342] to-[#512da8] min-h-screen">
       <Navbar />
+      <Homepage />
     </div>
   );
 }
